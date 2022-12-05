3a46b84a30667a46c867971653d3a2b92c636eeb
GabrielGuzAng <gabriel.guz.an@gmail.com>
2022-12-05 14:26:40 -0300

    upgrade

--- Telegram_lambda_Bot/lambda_function.py
+++ Telegram_lambda_Bot/lambda_function.py
@@ -2,9 +2,23 @@ import json
 import os
 import requests
 
-def lambda_handler(event,context):
-	telegram_token = os.environ['TELEGRAM_TOKEN']
+def check_param(event:dict, item:str, field_errors:dict)->str:
+    # checks for key and if val is null or an empty string
+    if (item in event) and (event[item]) and (str(event[item]).strip()) and event:
+        return (event[item])
+    else:
+        field_errors[item] = "Required"
+        return None
 
+
+def lambda_handler(event,context):
+	
+	field_errors={}
+	message = check_param(event, 'message', field_errors)
+	if field_errors:
+		raise Exception(json.dumps({'field_errors': field_errors}))
+
+	telegram_token = os.environ['TELEGRAM_TOKEN']
 	api_url = f"https://api.telegram.org/bot{telegram_token}/"
 	telegram_msg="Hi! May i help you?"
 	chat_id=os.environ['CHAT_ID']
