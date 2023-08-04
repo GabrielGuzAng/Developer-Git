b99f95996cd6845c246618afb38b88704877f519
GabrielGuzAng <gabriel.guz.an@gmail.com>
2023-08-04 19:04:36 -0300

    file app deleted

--- Empleado/App.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-
-from Lib_Empleado import Empleado
-from Lib_Tarjeta import Tarjeta
-
-t1=Tarjeta(input("\n Enter your card number please \n"))
-e1=Empleado((input("\n Enter your name please \n")),t1)
-e1.fichar()
